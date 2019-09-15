5fb00a35b71c379fcbe7354c05f7b632e881cbd8
Iku Iwasa <iku.iwasa@gmail.com>
2019-09-15 14:30:33 +0900

    Include english dictionaries in the artifact

---
+++ build-dict.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+set -ex
+
+DICT_NAME=aspell6-en-2018.04.16-0
+
+curl -LO "https://ftp.gnu.org/gnu/aspell/dict/en/$DICT_NAME.tar.bz2"
+tar xjf "$DICT_NAME.tar.bz2"
+
+cd "$DICT_NAME"
+
+cat > Makefile <<EOF
+ASPELL = C:/Aspell/bin/aspell
+ASPELL_FLAGS =
+PREZIP = C:/Aspell/bin/prezip-bin
+DESTDIR =
+dictdir = C:/Aspell/lib/aspell-0.60
+datadir = C:/Aspell/lib/aspell-0.60
+EOF
+
+cat Makefile.pre >> Makefile
+
+make install
